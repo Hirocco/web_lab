497a5ad650778c1b675dfd4cdf70fb98150ea424
Kamil Uchwat <92688578+Hirocco@users.noreply.github.com>
2024-10-10 07:38:58 +0000

    init

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-(function () {
-  //TODO
-})();
+const button1 = document.getElementById("ex1_button");
+
+button1.addEventListener("click", function () {
+  document.getElementById("ex1_content").innerHTML = "1,2,3,4,5,6,7,8,9";
+});
+
+const input1 = document.getElementById("ex2_text");
+
+input1.addEventListener("input", function () {
+  const outputInfo = document.getElementById("ex2_content");
+  if (input1.textContent !== 9) {
+    outputInfo.innerHTML = "Only 9 digits allowed";
+  } else if (input1.textContent != /[0-9]*/) {
+    outputInfo.innerHTML = "Only digits allowed";
+  }
+});
