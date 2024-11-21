44df246ccac3d55c2e73965b67860489f6aeff39
JanSzczepanski77 <janek.sz.03@gmail.com>
2024-11-21 09:03:24 +0100

    Lab4 and 1 initial upload

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+// define endpoint for exercise 1 here
+app.get('/math/circle/:r', (req, res) => {
+//TODO1
+  const r = parseFloat(req.params.r);
+
+  if(isNaN(r) || r <= 0) {
+    return res.status(400).json( { error: "Invalid radius" } );
+  }
+
+  const area = Math.PI * Math.pow(r, 2);
+  const obwod = 2 * Math.PI * r;
+
+  const result = {
+    area: area,
+    obwod: obwod
+  };
+
+  res.json(result);
+  console.log(result);
+});
+
+//TODO2
+
+
+//TODO3
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
