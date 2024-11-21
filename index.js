bfa974e547f9f12631521d2ce60966339bd0c3ad
JanSzczepanski77 <janek.sz.03@gmail.com>
2024-11-21 09:11:39 +0100

    Lab4_2

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.get('/math/circle/:r', (req, res) => {
   const obwod = 2 * Math.PI * r;
 
   const result = {
-    area: area,
-    obwod: obwod
+    area: area.toFixed(2),
+    obwod: obwod.toFixed(2)
   };
 
   res.json(result);
@@ -25,7 +25,24 @@ app.get('/math/circle/:r', (req, res) => {
 });
 
 //TODO2
+app.get('/math/rectangle/:width/:height', (req, res) => {
+  const w = parseFloat(req.params.width);
+  const h = parseFloat(req.params.height);
 
+  if(isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
+    return res.status(400).json( { error: "Invalid width or/and height" } ); 
+  }
+
+  const area = w * h;
+  const obwod = 2 * w + 2 * h;
+
+  const result = {
+    area: area.toFixed(2),
+    obwod: obwod.toFixed(2)
+  }
+  res.json(result);
+  console.log(result);
+});
 
 //TODO3
 
